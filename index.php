0504402d41c34958e03ee37768f47a1a0ea9757b
granumofficial <granumofficial@gmail.com>
2016-12-19 10:02:47 +0200

    Added index php for connected app in facebook
    Facebook makes POST request, not get - so index.html can't handle it

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<title>Granum Player</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="viewport" content="width=device-width">
+	</head>
+	</body>
+
+	<div id="app"></div>
+	
+	<script src="vendor.js"></script>
+	<script src="app.js"></script>
+	</body>
+</html>
